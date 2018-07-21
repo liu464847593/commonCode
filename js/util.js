b5edf31a8661ae1214c49e313062834167185e1d
Jeason <464847593@qq.com>
2018-07-21 10:26:12 +0800

    新增utilJs 用于常用的js

---
+++ js/util.js
@@ -0,0 +1,11 @@
+/**
+ *获取url的指定参数
+ * @param name url的参数名称
+ * @returns {*}
+ */
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
